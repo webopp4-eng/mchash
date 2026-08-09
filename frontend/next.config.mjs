2f5e03ade5c851a04426c7bd899f13c94514eecb
petersgodspower. <petersgodspower4@gmail.com>
2026-08-09 05:05:31 +0000

    Configure GitHub Pages deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,14 @@
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? '/mchash' : '',
+  assetPrefix: isGithubPages ? '/mchash/' : '',
 };
 
 export default nextConfig;
